be6bd2cdc95a4c7fb3a2b270126ef5907b6d193c
Fuyao Zhao <zfy0701@gmail.com>
2023-10-15 10:47:01 -0700

    migrate sentio tracer to geth
    support trace call many in geth
    
    migrate override and gas changes (#1)
    
    migrate memory compression (#2)
    
    add more information for root trace (#3)
    
    correct handle call with value (#4)
    
    * correct handle call with value
    
    * set transfer value to zero if can't transfer
    
    Add Mapping keys to post account (#5)
    
    fix when tracer failed before start (#6)
    
    unlimited gas for debug_traceCallMany (#7)
    
    support multiple txs in tracecallmany
    
    rpc client should keep result while return error
    
    be able to return partial results
    
    migrate tracer changes (#8)
    
    export meq field (#9)
    
    ignore init code size limit (#10)
    
    code address field (#11)

--- core/state_transition.go
+++ core/state_transition.go
@@ -339,14 +339,16 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	)
 
 	// Check clauses 4-5, subtract intrinsic gas if everything is correct
-	gas, err := IntrinsicGas(msg.Data, msg.AccessList, contractCreation, rules.IsHomestead, rules.IsIstanbul, rules.IsShanghai)
-	if err != nil {
-		return nil, err
+	if !st.evm.Config.IgnoreGas {
+		gas, err := IntrinsicGas(msg.Data, msg.AccessList, contractCreation, rules.IsHomestead, rules.IsIstanbul, rules.IsShanghai)
+		if err != nil {
+			return nil, err
+		}
+		if st.gasRemaining < gas {
+			return nil, fmt.Errorf("%w: have %d, want %d", ErrIntrinsicGas, st.gasRemaining, gas)
+		}
+		st.gasRemaining -= gas
 	}
-	if st.gasRemaining < gas {
-		return nil, fmt.Errorf("%w: have %d, want %d", ErrIntrinsicGas, st.gasRemaining, gas)
-	}
-	st.gasRemaining -= gas
 
 	// Check clause 6
 	if msg.Value.Sign() > 0 && !st.evm.Context.CanTransfer(st.state, msg.From, msg.Value) {
@@ -354,7 +356,7 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	}
 
 	// Check whether the init code size has been exceeded.
-	if rules.IsShanghai && contractCreation && len(msg.Data) > params.MaxInitCodeSize {
+	if !st.evm.Config.IgnoreCodeSizeLimit && rules.IsShanghai && contractCreation && len(msg.Data) > params.MaxInitCodeSize {
 		return nil, fmt.Errorf("%w: code size %v limit %v", ErrMaxInitCodeSizeExceeded, len(msg.Data), params.MaxInitCodeSize)
 	}
 
